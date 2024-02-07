b44fc378ae66b47612092eeb44c44ade4e0117a9
Nancy <nancyfanous1@gmail.com>
2024-02-07 18:29:01 +0200

    test

--- polybot/bot.py
+++ polybot/bot.py
@@ -89,7 +89,7 @@ class ObjectDetectionBot(Bot):
 
             # TODO send message to the Telegram end-user (e.g. Your image is being processed. Please wait...)
             self.send_text(chat_id=msg['chat']['id'], text="Your image is being processed. Please wait...")
-            self.send_text(chat_id=msg['chat']['id'], text="Your image is being processed. Please wait...")
+
 
 
 
